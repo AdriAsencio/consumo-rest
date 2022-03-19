742a684dcf17147ad02aee642b1c71d622b19645
AdriAsencio <adriana.asencio.est@tecazuay.edu.ec>
2022-03-18 22:54:09 -0500

    Updates

---
+++ precache-manifest.f6976f11fc2652829e36e60f4b3fec13.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc32463d8e2b12bfadaee1cd79107ff",
+    "url": "/consumo-rest/index.html"
+  },
+  {
+    "revision": "3b605e45a2ff6395e844",
+    "url": "/consumo-rest/static/css/2.aa564478.chunk.css"
+  },
+  {
+    "revision": "c7c66a71d938db088015",
+    "url": "/consumo-rest/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3b605e45a2ff6395e844",
+    "url": "/consumo-rest/static/js/2.d31476a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/consumo-rest/static/js/2.d31476a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7c66a71d938db088015",
+    "url": "/consumo-rest/static/js/main.9826dfe8.chunk.js"
+  },
+  {
+    "revision": "02504f49fd114d92dc32",
+    "url": "/consumo-rest/static/js/runtime-main.471bd120.js"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "/consumo-rest/static/media/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "/consumo-rest/static/media/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "/consumo-rest/static/media/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "/consumo-rest/static/media/primeicons.e01fd413.eot"
+  }
+]);
